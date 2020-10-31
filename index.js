ffe06030b9ae9ac950d8464a815c555374faff58
Paus <mariemeline.paus@gmail.com>
2020-10-31 16:48:23 +0100

    ok

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000 || process.env.PORT;
-const dotenv = require('dotenv');
-const cors = require('cors');
-const routes = require('./routes');
+const port = process.env.PORT || 3000;
+const dotenv = require("dotenv");
+const cors = require("cors");
+const routes = require("./routes");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-app.use('/api', routes);
+app.use("/api", routes);
 
-
-app.listen(port, (err) => {
-  if(err) {
-    console.log(err)
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
   }
-  console.log('Super :)')
-})
-
-
+  console.log("Super :)");
+});
